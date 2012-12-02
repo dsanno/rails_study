96a20c950c60e8647d0cff8c381336ed146a3c16
Daiki Sannou <Daiki.Sannou@access-company.com>
2012-12-03 08:49:14 +0900

    create project.

---
+++ railbook/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railbook2::Application.config.secret_token = 'fb5b83a6d18b88dc349b2d66642089da7080b495f29ac3316ff5dd0ba6976ec818e4843fd509363b462a31a36e8b22544807d441c6922d743d06746880a26068'
