a1291d534aaab64f9797a7324cebab033fae2f75
Daiki Sannou <Daiki.Sannou@access-company.com>
2012-12-05 08:56:27 +0900

    bookデータベースを作成。

--- railbook/db/schema.rb
+++ railbook/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121204000515) do
+ActiveRecord::Schema.define(:version => 20121204235418) do
 
   create_table "books", :force => true do |t|
     t.string   "isbn"
